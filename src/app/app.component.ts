91e67595eda89f4375d1dbec09a65c1eec1aca7a
Isaac Shoman and Ted Leary <isaac@>
2018-05-02 13:49:04 -0700

    Display keg objects in cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Keg } from './models/keg.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  masterKegList: Keg[] = [
+    new Keg('Czech Pils', 'Buoy', 6, 6.2, 124, "../assets/buoy.jpg"),
+    new Keg('Pils', 'Heater Allen', 5, 5.2, 124, "../assets/heaterallen.png"),
+    new Keg('Gimme Mo', 'Crux', 4, 6.2, 124, "../assets/crux.jpg"),
+    new Keg('Oatmeal Pale Ale', 'Burnside Brewing', 6, 5.6, 124, "../assets/burnsidebrewing.jpg")
+  ];
 }
