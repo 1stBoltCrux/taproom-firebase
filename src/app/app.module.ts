c0d54eec7a20fac263bdf3b95ff25adb05836896
Isaac Shoman and Ted Leary <isaac+ted@>
2018-05-03 15:38:14 -0700

    take some steps toward enabling firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { KegListComponent } from './keg-list/keg-list.component';
 import { KegListEditComponent } from './keg-list-edit/keg-list-edit.component';
 import { KegAddComponent } from './keg-add/keg-add.component';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
@@ -15,11 +24,16 @@ import { KegAddComponent } from './keg-add/keg-add.component';
     KegListComponent,
     KegListEditComponent,
     KegAddComponent,
-
+    // MarketplaceComponent,
+    // AlbumDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    // HttpModule,
+    // routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
